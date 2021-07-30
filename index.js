a9519b8666bfabd73b5b9f35f4c61647d7510a20
Evgeniy Tkachuk <evgeniy.tkachuk@actionmediamtl.com>
2021-07-31 00:56:01 +0300

    Removed unnecessarily files

--- index.js
+++
@@ -1,3 +0,0 @@
-export { default as Cell } from './src/Cell';
-export { default as Field } from './src/Field';
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,2 +0,0 @@
-export {default as Cell} from './src/Cell'
-export {default as Field} from './src/Field'
