6fc421df64fc068e13455429ac1e4f28b6675846
DevilRep <flaringrep@gmail.com>
2021-10-04 21:31:57 +0300

    Updated decorator for cells

---
+++ src/AbstractCellDecorator.ts
@@ -0,0 +1,19 @@
+import Cell from './Cell';
+import ICell from './interfaces/Cell'
+
+export default abstract class AbstractCellDecorator extends Cell {
+    protected cell: ICell
+
+    constructor(cell: ICell) {
+        super(cell.position, cell.display)
+        this.cell = cell
+    }
+
+    async move(newPosition: number): Promise<void> {
+        return this.cell.move(newPosition);
+    }
+
+    async moveWhileShuffling(newPosition: number): Promise<void> {
+        return this.cell.moveWhileShuffling(newPosition);
+    }
+}
--- src/interfaces/CellDecorator.ts
+++
@@ -1,7 +0,0 @@
-export default interface CellDecorator {
-    readonly display: string
-    readonly position: number
-
-    move(newPosition: number): Promise<void>
-    moveWhileShuffling(newPosition: number): Promise<void>
-}
